1156e5ec5ee68b4bdea2313104024a76c3cf3c52
argonauttz <sasha.kozin.2014alex444@ya.ru>
2023-02-02 17:35:10 +0300

    Initial Commit

---
+++ CalculatorMVVMSwiftUI/CalculatorMVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorMVVMSwiftUIApp.swift
+//  CalculatorMVVMSwiftUI
+//
+//  Created by Alexandr Kozin on 02.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorMVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
